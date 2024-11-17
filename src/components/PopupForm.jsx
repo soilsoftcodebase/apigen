12ca17cdb8281ed6dd3ecd3c6fc46a3ac09377b3
Mano Sai Seelam <soilsoftgit+7@gmail.com>
2024-11-17 12:41:11 +0530

    Added colums in Runs

--- src/components/PopupForm.jsx
+++ src/components/PopupForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   createProject,
-  getTestCases,
   saveAndGenerateTestCases,
   fetchSwaggerInfo,
 } from "../Services/apiGenServices";
